741ad54b03ec770c68b143a37c7b4a036950d836
murzikkot978 <Roman.Yanivets@jobtrek.ch>
2024-09-27 15:20:50 +0200

    feat: requesting a task and write it in a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io;
+use std::io::Result;
+use std::io::Write;
+
+fn main() -> Result<()> {
+    let mut todo = String::new();
+    println!("Give me your todo");
+    io::stdin().read_line(&mut todo).expect("Read line failed");
+
+    let mut todo_list = File::create("todo_list.txt")?;
+
+    let _ = todo_list.write_all(todo.as_bytes());
+    todo_list.flush()?;
+
+    Ok(())
 }
