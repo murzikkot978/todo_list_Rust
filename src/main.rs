c271c458229da61b2b4c166c883a3e958c035627
murzikkot978 <Roman.Yanivets@jobtrek.ch>
2024-10-08 15:29:11 +0200

    feat: user can delete line in the file with the flag --delete (line)

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
-use std::fs::OpenOptions;
+use std::fs;
+use std::fs::read_to_string;
 use std::io;
-use std::io::Write;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let mut todo = String::new();
-    println!("Give me your todo");
-    io::stdin().read_line(&mut todo).expect("Read line failed");
+    println!("Give me your todo : ");
+    io::stdin()
+        .read_line(&mut todo)
+        .expect("Failed to read line");
 
     let todo = todo.trim();
+    //Open file.
+    let mut todos: Vec<String> = match read_to_string("todo_list.txt") {
+        Err(_) => Vec::new(),
+        Ok(todo_list) => todo_list.lines().map(String::from).collect(),
+    };
 
-    let mut todo_list = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .append(true)
-        .create(true)
-        .open("todo_list.txt")
-        .expect("C'est pas posible");
+    //Check if the user typed --delete.
 
-    writeln!(todo_list, "{}", todo).expect("afsadva");
+    if todo.contains("--delete") {
+        //Find the last value.
+        let test = todo.split(" ").last();
+        let nombre_line: usize = test.expect("Err").parse().unwrap();
+
+        todos.remove(nombre_line - 1);
+    } else {
+        todos.push(todo.to_string());
+    }
+
+    fs::write("todo_list.txt", todos.join("\n")).expect("can't write to file!");
+
+    Ok(())
 }
