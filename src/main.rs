a3aa167ce45c3556033f500f0d6765d8f84a410b
murzikkot978 <Roman.Yanivets@jobtrek.ch>
2024-10-22 10:13:47 +0200

    feat!: storage format changed from text to JSON

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,51 @@
-use std::fs;
+use serde::{Deserialize, Serialize};
+use serde_json::{self};
 use std::fs::read_to_string;
+use std::fs::{self};
 use std::io;
 
+#[derive(Serialize, Deserialize)]
+
+struct Todo {
+    contents: String,
+}
+
 fn main() -> std::io::Result<()> {
-    let mut todo = String::new();
+    let mut message = String::new();
     println!("Give me your todo : ");
     io::stdin()
-        .read_line(&mut todo)
+        .read_line(&mut message)
         .expect("Failed to read line");
 
-    let todo = todo.trim();
+    let message = message.trim();
+
+    let todo = Todo {
+        contents: message.to_string(),
+    };
+
     //Open file.
-    let mut todos: Vec<String> = match read_to_string("todo_list.txt") {
+    let mut todos: Vec<Todo> = match read_to_string("todo.json") {
         Err(_) => Vec::new(),
-        Ok(todo_list) => todo_list.lines().map(String::from).collect(),
+        Ok(todos) => serde_json::from_str(&todos).expect("can't open file todo.json"),
     };
 
     //Check if the user typed --delete.
 
-    if todo.contains("--delete") {
+    if message.contains("--delete") {
         //Find the last value.
-        let test = todo.split(" ").last();
-        let nombre_line: usize = test.expect("Err").parse().unwrap();
+        let test = message.split(" ").last();
+        let nombre_line: usize = test.expect("Cannot split").parse().unwrap();
 
         todos.remove(nombre_line - 1);
     } else {
-        todos.push(todo.to_string());
+        todos.push(todo);
     }
 
-    fs::write("todo_list.txt", todos.join("\n")).expect("can't write to file!");
+    fs::write(
+        "todo.json",
+        serde_json::to_string(&todos).expect("can't write to file!"),
+    )
+    .expect("can't write file");
 
     Ok(())
 }
