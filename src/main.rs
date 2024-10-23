0e34343d5f2da00acfcf763282bf4b7f08db5db6
murzikkot978 <Roman.Yanivets@jobtrek.ch>
2024-10-23 14:24:14 +0200

    feat: users can delete tasks directly from the command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use serde::{Deserialize, Serialize};
 use serde_json::{self};
 use std::fs::read_to_string;
@@ -10,35 +11,47 @@ struct Todo {
     contents: String,
 }
 
+#[derive(Parser)]
+struct Flag {
+    /// Write flag
+    #[arg(long, short, default_value_t = 0)]
+    delete: usize,
+}
+
 fn main() -> std::io::Result<()> {
-    let mut message = String::new();
-    println!("Give me your todo : ");
-    io::stdin()
-        .read_line(&mut message)
-        .expect("Failed to read line");
-
-    let message = message.trim();
-
-    let todo = Todo {
-        contents: message.to_string(),
-    };
-
     //Open file.
     let mut todos: Vec<Todo> = match read_to_string("todo.json") {
         Err(_) => Vec::new(),
         Ok(todos) => serde_json::from_str(&todos).expect("can't open file todo.json"),
     };
 
-    //Check if the user typed --delete.
+    let flag: Flag = Flag::parse();
+    if flag.delete > 0 && flag.delete <= todos.len() {
+        todos.remove(flag.delete - 1);
 
-    if message.contains("--delete") {
-        //Find the last value.
-        let test = message.split(" ").last();
-        let nombre_line: usize = test.expect("Cannot split").parse().unwrap();
-
-        todos.remove(nombre_line - 1);
+    
     } else {
-        todos.push(todo);
+        let mut message = String::new();
+        println!("Give me your todo : ");
+        io::stdin()
+            .read_line(&mut message)
+            .expect("Failed to read line");
+
+        let message = message.trim();
+
+        let todo = Todo {
+            contents: message.to_string(),
+        };
+
+        if message.contains("--delete") {
+            //Find the last value.
+            let test = message.split(" ").last();
+            let nombre_line: usize = test.expect("Cannot split").parse().unwrap();
+    
+            todos.remove(nombre_line - 1);
+        } else {
+            todos.push(todo);
+        }
     }
 
     fs::write(
