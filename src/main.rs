b5982ab5454fcb6e251d10929dea5e44232a765c
murzikkot978 <Roman.Yanivets@jobtrek.ch>
2024-10-24 08:22:48 +0200

    feat: user can assign the status of done to the task

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,21 @@ use std::io;
 
 struct Todo {
     contents: String,
+    status: bool,
 }
 
 #[derive(Parser)]
 struct Flag {
-    /// Write flag
-    #[arg(long, short, default_value_t = 0)]
+    /// Write nombre line
+    #[arg(long, default_value_t = 0)]
     delete: usize,
+
+    /// Write nombre line
+    #[arg(long, default_value_t = 0)]
+    done: usize,
 }
 
-fn main() -> std::io::Result<()> {
+fn main() {
     //Open file.
     let mut todos: Vec<Todo> = match read_to_string("todo.json") {
         Err(_) => Vec::new(),
@@ -28,8 +33,9 @@ fn main() -> std::io::Result<()> {
     let flag: Flag = Flag::parse();
     if flag.delete > 0 && flag.delete <= todos.len() {
         todos.remove(flag.delete - 1);
-
-    
+        println!("Tâche supprimée.");
+    } else if flag.done > 0 && flag.done <= todos.len() {
+        todos[flag.done - 1].status = true;
     } else {
         let mut message = String::new();
         println!("Give me your todo : ");
@@ -41,13 +47,14 @@ fn main() -> std::io::Result<()> {
 
         let todo = Todo {
             contents: message.to_string(),
+            status: false,
         };
 
         if message.contains("--delete") {
             //Find the last value.
             let test = message.split(" ").last();
             let nombre_line: usize = test.expect("Cannot split").parse().unwrap();
-    
+
             todos.remove(nombre_line - 1);
         } else {
             todos.push(todo);
@@ -59,6 +66,4 @@ fn main() -> std::io::Result<()> {
         serde_json::to_string(&todos).expect("can't write to file!"),
     )
     .expect("can't write file");
-
-    Ok(())
 }
